8873ffeee5cae58e12f09b5733321519908a51b6
omarakamal <omaraakamal@gmail.com>
2025-11-30 20:08:06 +0300

    update

--- app.js
+++ app.js
@@ -79,3 +79,74 @@ const bestMoviesObjects = movies.filter(function(oneMovieObject){
 
 
 console.log(bestMoviesObjects)
+
+
+const shoppingCart = [
+    {
+        name: "Laptop",
+        price: 1200,
+        shipsToBahrain: true
+    },
+    {
+        name: "Smartphone",
+        price: 800,
+        shipsToBahrain: false
+    },
+    {
+        name: "Headphones",
+        price: 150,
+        shipsToBahrain: true
+    },
+    {
+        name: "Smartwatch",
+        price: 300,
+        shipsToBahrain: false
+    }
+];
+
+
+const below200Items = shoppingCart.findIndex(function(oneItem){
+    if(oneItem.price < 200){
+        return true
+    }
+})
+
+console.log(below200Items)
+
+
+
+// every() returns true if every item in the array matches the condition
+
+
+// some(): returns true if at least one element matches the condition
+const isShoppingCartValid = shoppingCart.some(function(oneItem){   
+    if(oneItem.shipsToBahrain){
+        return true
+    }
+})
+
+
+console.log(isShoppingCartValid)
+
+
+
+
+
+
+
+const studentsArr = ['Taha','Sayed','Mohammad','Ahmad','Sarah']
+
+// for strings
+
+studentsArr.sort()
+
+console.log(studentsArr)
+
+
+const numbersArr = [5, 1, 10, 15, 20, 2, 5, 55]
+
+numbersArr.sort(function(a,b){
+    return b-a
+})
+
+console.log(numbersArr)
